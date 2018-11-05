ee6220eccdccaf4c00047fef8c198391ef9371f7
Alessandro Bianchi <alex24br@yahoo.it>
2018-11-05 12:49:30 +0100

    implementato esercizio

--- src/main/java/ch/supsi/webapp/web/BlogPostController.java
+++ src/main/java/ch/supsi/webapp/web/BlogPostController.java
@@ -2,37 +2,79 @@ package ch.supsi.webapp.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class BlogPostController
-{
+public class BlogPostController {
     private List<BlogPost> posts = new ArrayList<>();
 
     @RequestMapping(value = "/blogposts", method = RequestMethod.GET)
-    public List<BlogPost> get()
-    {
+    public List<BlogPost> getAll() {
         //System.out.println("");
         return posts;
     }
 
+    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.GET)
+    public ResponseEntity<BlogPost> getPost(@PathVariable int id) {
+        for (BlogPost b : posts) {
+            if (b.getId() == id)
+                return new ResponseEntity<>(b, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>((BlogPost) null, HttpStatus.NOT_FOUND);
+    }
+
     @RequestMapping(value = "/blogposts", method = RequestMethod.POST)
-    public ResponseEntity<BlogPost> post(@RequestBody BlogPost post)
-    {
-        if((post.getAuthor() != null) && (post.getTitle() != null) && (post.getText() != null))
-        {
+    public ResponseEntity<BlogPost> post(@RequestBody BlogPost post) {
+        if ((post.getAuthor() != null) && (post.getTitle() != null) && (post.getText() != null)) {
             post.setId(posts.size());
             System.out.print("ciao");
             posts.add(post);
             return new ResponseEntity<>(post, HttpStatus.OK);
-        }
-        else
+        } else
             return new ResponseEntity<>(post, HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<BlogPost> put(@RequestBody BlogPost post, @PathVariable int id)
+    {
+        if (!((post.getAuthor() != null) && (post.getTitle() != null) && (post.getText() != null)))
+            return new ResponseEntity<>((BlogPost) null, HttpStatus.BAD_REQUEST);
+
+        for(BlogPost b : posts)
+        {
+            if(b.getId() == id)
+            {
+                {
+                    b.setId(post.getId());
+                    b.setAuthor(post.getAuthor());
+                    b.setTitle(post.getTitle());
+                    b.setText(post.getText());
+
+                    return new ResponseEntity<>(b, HttpStatus.OK);
+                }
+            }
+        }
+
+        return new ResponseEntity<>((BlogPost) null, HttpStatus.NOT_FOUND);
+    }
+
+
+    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> delete(@PathVariable int id)
+    {
+        for(BlogPost b : posts)
+        {
+            if (b.getId() == id)
+            {
+                posts.remove(b);
+                return new ResponseEntity<>("\"success\": true", HttpStatus.OK);
+            }
+        }
+
+        return new ResponseEntity<>((String) null, HttpStatus.NOT_FOUND);
     }
 }
